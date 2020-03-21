439ca475dbed98ecc322fd56e375967d16b053a2
talisonf <talison7737@gmail.com>
2020-03-21 18:36:38 -0300

    Join data frames and plot grafh

--- AnaliseFundamentalista.py
+++ AnaliseFundamentalista.py
@@ -3,12 +3,14 @@ import pandas as pd
 from pandas import DataFrame
 import pprint
 import matplotlib.pyplot as plt
-empresa = 'AAPL'
-#req = HistoricalPrices(empresa, '2007-01-01', '2020-01-01').to_csv(path="mercado"+empresa+".csv", sep=', ', data_format='raw', csv_dialect='excel')
-df = pd.read_csv("mercado"+empresa+".csv", encoding="UTF-8", sep=",")
-print(df.head())
-print(df.tail())
-
-df.plot(x='Date', y='Close', kind='line')
+empresas = ['AMZN','TSLA','FB']
+dfs = []
+for empresa in empresas:
+    req = HistoricalPrices(empresa, '2019-03-20','2020-03-20').to_csv(path="mercado"+empresa+".csv", sep=', ', data_format='raw', csv_dialect='excel')
+    dfs.append(pd.read_csv("mercado"+empresa+".csv", encoding="UTF-8", sep=","))
+    pass
+df = pd.concat(dfs)
+print(df.shape)
+dfJoin = df[['Date','Close']].groupby(['Date']).mean().plot()
 plt.show()
-
+    
