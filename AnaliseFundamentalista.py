901a94f907007b2f2b2c4254de7dafb957317577
talisonf <talison7737@gmail.com>
2020-03-21 13:05:49 -0300

    git ignore

--- AnaliseFundamentalista.py
+++ AnaliseFundamentalista.py
@@ -3,10 +3,12 @@ import pandas as pd
 from pandas import DataFrame
 import pprint
 import matplotlib.pyplot as plt
-
-#req = HistoricalPrices('AAPL', '2000-01-01', '2020-01-01').to_csv(path="mercado.csv", sep=', ', data_format='raw', csv_dialect='excel')
-df = pd.read_csv("mercado.csv", encoding="UTF-8", sep=",")
+empresa = 'AAPL'
+#req = HistoricalPrices(empresa, '2007-01-01', '2020-01-01').to_csv(path="mercado"+empresa+".csv", sep=', ', data_format='raw', csv_dialect='excel')
+df = pd.read_csv("mercado"+empresa+".csv", encoding="UTF-8", sep=",")
 print(df.head())
+print(df.tail())
+
 df.plot(x='Date', y='Close', kind='line')
 plt.show()
 
