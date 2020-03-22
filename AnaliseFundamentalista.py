f287f48e86847673ff65253a1c44237f5d1563d7
talisonf <talison7737@gmail.com>
2020-03-22 18:58:12 -0300

    add time and read parterns from file

--- AnaliseFundamentalista.py
+++ AnaliseFundamentalista.py
@@ -3,14 +3,27 @@ import pandas as pd
 from pandas import DataFrame
 import pprint
 import matplotlib.pyplot as plt
-empresas = ['AMZN','TSLA','FB']
+import csv
+import timeit
+
+
+
+
+with open('empresas', newline='') as f:
+    reader = csv.reader(f)
+    empresas = list(reader)
+
 dfs = []
 for empresa in empresas:
-    req = HistoricalPrices(empresa, '2019-03-20','2020-03-20').to_csv(path="mercado"+empresa+".csv", sep=', ', data_format='raw', csv_dialect='excel')
-    dfs.append(pd.read_csv("mercado"+empresa+".csv", encoding="UTF-8", sep=","))
+    print(empresa[1])
+    req = HistoricalPrices(empresa[1], '1990-03-20','2020-03-21').to_csv(path="mercado"+empresa[1]+".csv", sep=', ', data_format='raw', csv_dialect='excel')
+    dfs.append(pd.read_csv("mercado"+empresa[1]+".csv", encoding="UTF-8", sep=","))
     pass
+
+start = timeit.default_timer()
 df = pd.concat(dfs)
+dfJoin = df[['Date','Close']].groupby(['Date']).sum().plot()
 print(df.shape)
-dfJoin = df[['Date','Close']].groupby(['Date']).mean().plot()
+stop = timeit.default_timer()
+print('Time: ', stop - start)  
 plt.show()
-    
